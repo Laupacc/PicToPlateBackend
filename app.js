45a5ebf03b7205a4694016c6358540cdc01221f9
Your name <Your email>
2024-08-21 16:47:02 +0200

    lots

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 require('dotenv').config();
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const helmet = require('helmet');
 
 require("./models/connection");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var recipesRouter = require('./routes/recipes');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const recipesRouter = require('./routes/recipes');
 
-var app = express();
-const cors = require('cors');
+const app = express();
 app.use(cors());
+app.use(helmet());
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 const USER_ID = process.env.USER_ID;
 const APP_ID = process.env.APP_ID;
@@ -12,44 +9,51 @@ const PAT = process.env.PAT;
 const MODEL_ID = process.env.MODEL_ID;
 const MODEL_VERSION_ID = process.env.MODEL_VERSION_ID;
 
+
 router.post('/classifyImage', async (req, res) => {
-  try {
     const { imageBase64 } = req.body;
 
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: USER_ID,
-        app_id: APP_ID,
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              base64: imageBase64,
+    try {
+        const raw = JSON.stringify({
+            user_app_id: {
+                user_id: USER_ID,
+                app_id: APP_ID,
             },
-          },
-        },
-      ],
-    });
+            inputs: [
+                {
+                    data: {
+                        image: {
+                            base64: imageBase64,
+                        },
+                    },
+                },
+            ],
+        });
 
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Key ${PAT}`,
-        "Content-Type": "application/json",
-      },
-      body: raw,
-    };
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                Authorization: "Key " + PAT,
+                "Content-Type": "application/json",
+            },
+            body: raw,
+        };
 
-    const result = await fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`, requestOptions);
-    const resultJson = await result.json();
+        const result = await fetch(
+            `https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`,
+            requestOptions
+        );
 
-    res.json(resultJson);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error classifying image');
-  }
+        const resultJson = await result.json();
+        console.log("Response from Clarifai:", resultJson);
+
+        const predictions = resultJson.outputs[0].data.concepts;
+        res.json(predictions);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 module.exports = router;
